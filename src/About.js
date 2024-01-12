c2bdcc8a8ad39297f3236cd64332d2d97e63a02c
faizanhip12 <113345962+faizanhip12@users.noreply.github.com>
2024-01-12 23:28:19 +0500

    changesSaved

--- src/About.js
+++ src/About.js
@@ -1,19 +1,20 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
-import { useEffect, useState, createContext ,useContext} from "react";
-import {AppContext} from './context/productcontex'
-import {useProductContext} from './context/productcontex'
+import { useEffect, useState, createContext, useContext } from "react";
+import { AppContext } from './context/productcontex'
+import { useProductContext } from './context/productcontex'
 const About = () => {
-  const myName =useContext(AppContext)
+  // const myName =useContext(AppContext)
+  const myName = useProductContext()
   const data = {
-    name: "Thapa Ecommerce",
+    name: "Test",
   };
 
-  return<>
-  {myName}
-   <HeroSection myData={data} />
+  return <>
+    { myName}
+    <HeroSection myData={data} />
 
   </>
-  
+
 }
 export default About
