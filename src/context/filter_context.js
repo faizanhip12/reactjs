ae70db29a7c197c3a5b15065be1e0c331694659a
faizanhip12 <113345962+faizanhip12@users.noreply.github.com>
2024-01-19 19:14:50 +0500

    changesSaved

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -7,32 +7,52 @@ const FilterContext = createContext();
 const initialState = {
     filter_products: [],
     all_products: [],
-    grid_view: true
-    
+    grid_view: true,
+    sorting_value:"lowest",
+    filters: {
+        text: "",
+        category: "all",
+        company: "all",
+    },
+
 }
 
 export const FilterContextProvider = ({ children }) => {
-    const {products} = useProductContext()
+    const { products } = useProductContext()
     const [state, dispatch] = useReducer(reducer, initialState)
 
-    console.log("productsproductsproducts",products)
+    console.log("productsproductsproducts", products)
 
     const setGridView = () => {
+        console.log("setGridViewsetGridViewsetGridView")
         return dispatch({ type: "SET_GRID_VIEW" });
-      };
-    
-      // to set the list view
-      const setListView = () => {
+    };
+
+    // to set the list view
+    const setListView = () => {
+        console.log("setListViewsetListViewsetListViewsetListView")
         return dispatch({ type: "SET_LIST_VIEW" });
-      };
-    
+    };
+
+    //sorting function
+
+    const sorting = () => {
+        console.log("sorting")
+        dispatch({type:"GET_SORT_VALUE"})
+    }
 
 
     useEffect(() => {
         dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products })
     }, [products])
 
-    return <FilterContext.Provider value={{ ...state }}>
+    return <FilterContext.Provider value={{
+        ...state,
+        setGridView,
+        setListView,
+        sorting
+
+    }}>
         {children}
     </FilterContext.Provider>
 }
--- src/reducer/filter_reducer.js
+++ src/reducer/filter_reducer.js
@@ -1,24 +1,37 @@
 const filterReducer = (state, action) => {
-    console.log("action.payload",action.payload)
- 
-switch (action.type) {
-    case "LOAD_FILTER_PRODUCTS":
-        return {
-            ...state,
-            filter_products:[...action.payload],
-            allproducts:[...action.payload],
-        }
+    console.log("action.payload", action.payload)
 
-    case "SET_GRID_VIEW":
-        console.log("SET_GRID_VIEWSET_GRID_VIEWSET_GRID_VIEW")
-        return{
-            ...state,
-            grid_view:true
-        }    
-    
+    switch (action.type) {
+        case "LOAD_FILTER_PRODUCTS":
+            return {
+                ...state,
+                filter_products: [...action.payload],
+                allproducts: [...action.payload],
+            }
 
-    default:return state;
-}
+        case "SET_GRID_VIEW":
+            console.log("SET_GRID_VIEWSET_GRID_VIEWSET_GRID_VIEW")
+            return {
+                ...state,
+                grid_view: true
+            }
+        case "SET_LIST_VIEW":
+            return {
+                ...state,
+                grid_view: false,
+            };
+
+
+        case "GET_SORT_VALUE":
+            let userSortValue = document.getElementById("sort")
+            let sort_value =userSortValue.options[userSortValue.selectedIndex].value
+            console.log("sort_valuesort_valuesort_valuesort_value")
+            return {
+            ...state,
+            sorting_value:sort_value,
+            }
+        default: return state;
+    }
 
 }
 
